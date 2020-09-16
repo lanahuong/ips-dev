b1ae35c384ac29bb5d51fe6826907f4e883ee018
Michel MIGDAL <michel.migdal@ensiie.fr>
2020-09-16 20:58:22 +0200

    Test mineur pour voir si tout compile bien

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include "main.h"
-
+#include "hermite.h"
 using namespace std;
 
 int main(int argc, char** argv) {
 
   cout << "Hello world" << endl;
+
+  Hermite::ComputeMatrix(30, 0, 5, 0.1).print("The Matrix");
 
   return 0;
 }
