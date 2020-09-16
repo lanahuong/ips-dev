b147c0fa5396ffb9eb4434fbbef4aff2cbfd4aa2
Michel MIGDAL <michel.migdal@ensiie.fr>
2020-09-16 21:49:28 +0200

    changement de structure

--- src/hermite.cpp
+++ src/hermite.cpp
@@ -16,19 +16,20 @@ arma::mat Hermite::ComputeMatrix(int n_max, double z_start, double z_end, double
     /**
      * If the parameters are nonsense the matrix (0) is returned
      */
+    arma::mat herm_out ;
     if (n_max < 0 || step <= 0 || z_start >= z_end) {
-        return arma::mat(1, 1, arma::fill::zeros);
-    }
-
-    arma::vec z = arma::regspace(z_start, step, z_end);
-    int n = z.size();
-    arma::mat m = arma::mat(n_max + 1, n, arma::fill::zeros);
-    m.row(0) = arma::vec(n, arma::fill::ones).t();
-    if (n_max > 0) {
-        m.row(1) = 2 * z.t();
-        for (int i = 2; i <= n_max; i++) {
-            m.row(i) = m.row(1) % m.row(i - 1) - 2 * (i-1) * m.row(i - 2);
+        herm_out = arma::mat(1, 1, arma::fill::zeros);
+    } else {
+        arma::vec z = arma::regspace(z_start, step, z_end);
+        int n = z.size();
+        herm_out = arma::mat(n_max + 1, n, arma::fill::zeros);
+        herm_out.row(0) = arma::vec(n, arma::fill::ones).t();
+        if (n_max > 0) {
+            herm_out.row(1) = 2 * z.t();
+            for (int i = 2; i <= n_max; i++) {
+                herm_out.row(i) = herm_out.row(1) % herm_out.row(i - 1) - 2 * (i - 1) * herm_out.row(i - 2);
+            }
         }
     }
-    return m;
+    return herm_out;
 }
