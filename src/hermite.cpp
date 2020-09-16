4930d465ef849e0b3ae8de732c484f42c45b422c
Michel MIGDAL <michel.migdal@ensiie.fr>
2020-09-16 12:44:18 +0200

    Pour hermite.cpp

---
+++ src/hermite.cpp
@@ -0,0 +1,9 @@
+//
+// Created by migda on 16/09/2020.
+//
+
+#include "hermite.h"
+
+Hermite::Hermite(int) {
+
+}
---
+++ src/hermite.h
@@ -0,0 +1,26 @@
+//
+// Created by migda on 16/09/2020.
+//
+
+#ifndef PROJET_IPS1_HERMITE_H
+#define PROJET_IPS1_HERMITE_H
+
+
+
+#include <armadillo>
+#include <vector>
+
+class Hermite{
+public:
+    Hermite(int);
+private :
+    int n_max;
+};
+
+
+
+
+
+
+
+#endif //PROJET_IPS1_HERMITE_H
