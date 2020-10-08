06f88b9496787a71c7b1a554346976fb35f5c4ee
Lana Scravaglieri <lana.scravaglieri@gmail.com>
2020-10-08 12:35:29 +0200

    The plotter in python to plot the functions

---
+++ src/plotter/plotter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import csv
+import matplotlib.pyplot as pp
+
+functions = []
+z = []
+
+with open('test.csv', newline='') as csvfile:
+    funcreader = csv.reader(csvfile, delimiter=',', quoting=csv.QUOTE_NONNUMERIC)
+    i = 0
+    for row in funcreader:
+        if i == 0:
+            z = row
+        else:
+            functions.append(row)
+        i+=1
+
+for i in range(len(functions)):
+    pp.plot(z,functions[i])
+
+pp.show()
