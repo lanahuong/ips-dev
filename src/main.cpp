75aabda4277f98511bc6364ae16972286393446c
Lana Scravaglieri <lana.scravaglieri@gmail.com>
2020-10-08 20:09:02 +0200

    Save the results to CSV and refactoring of main.c

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,20 @@
-#include "hermite.h"
-#include "solverschrodinger.h"
-#include "check_orthogonality.h"
+#include "main.h"
 
 using namespace std;
 
-
 int main() {
-    arma::mat m = solverSchrodinger::solve1D(0, 1, 1);
+    double bound = 7;
+    int n = 3;
+    arma::mat m = solverSchrodinger::solve1D(-bound, bound, n);
     m.print("The Matrix");
 
     auto *checker = new orthogonalityChecker(5);
     cout << checker->checkFor(4, 4) << '\n';
     cout << checker->checkFor(2, 4) << '\n';
     cout << checker->checkFor(1, 4) << '\n';
+
+    arma::rowvec z = arma::regspace(-bound, STEP, bound).as_row();
+    Saver::saveToCSV(z, m);
 
     return 0;
 }
