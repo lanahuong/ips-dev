fed80c95597355cc349e89368edf7f76854ebfdf
Michel MIGDAL <michel.migdal@ensiie.fr>
2020-10-11 22:11:57 +0200

    Doxydoc in constants.h

--- src/constants.h
+++ src/constants.h
@@ -6,12 +6,12 @@
 #define PROJET_IPS1_CONSTANTS_H
 
 
-#define EPSILON 0.0001
-#define H_BAR 1.0
-#define MASS 1.0
-#define OMEGA 1.0
-#define STEP 0.1
-#define PI 3.14159 //<! needed to normalize the quadrature
+#define EPSILON 0.0001 ///< Used as a treshold to check wether a double is null or not.
+#define H_BAR 1.0 ///< Reduced Planck constant
+#define MASS 1.0 ///< The mass
+#define OMEGA 1.0 ///< Angular momentum
+#define STEP 0.1 ///< The step used in arma::regspace
+#define PI 3.14159 ///< Real value needed to normalize the scalar product else the kroneckers wont be equals to 1
 
 
 #endif
