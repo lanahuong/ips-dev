e3c627f52203e773583ee0a5286587261f4f2a34
Michel MIGDAL <michel.migdal@ensiie.fr>
2020-10-13 19:39:35 +0200

    Profiling of the code

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "main.h"
 
+
 using namespace std;
 
 int main()
@@ -7,10 +8,10 @@ int main()
   double bound = 7;
   uint n = 3;
   arma::mat m = SolverSchrodinger::solve1D(-bound, bound, n);
+
+#ifdef DEBUG
   m.print("The Matrix");
-
   auto *checker = new OrthogonalityChecker();
-
   for (uint i = 0; i < HERM_QUADRA_N_MAX; i++)
     {
       for (uint j = 0; j < HERM_QUADRA_N_MAX; j++)
@@ -18,14 +19,18 @@ int main()
           cout << "params: " << i << " " << j << "res: " << checker->checkFor(i, j) << '\n';
         }
     }
-  free(checker);
-
+  delete checker;
+#endif
   arma::rowvec z = arma::regspace(-bound, STEP, bound).as_row();
+#ifdef DEBUG
   bool test = SolverSchrodinger::test1DSolution(z, m);
-
   cout << to_string(test);
-
+  if {!test}{
+    return 1;
+  }
+#endif
   Saver::saveToCSV(z, m);
+  cout << "file saved!" << endl;
 
   return 0;
 }
